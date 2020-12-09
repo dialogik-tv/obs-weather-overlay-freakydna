9b4569a9f8467774d83df2f2936b92516eb487e0
Moritz Hesse <mh@interhesse.com>
2020-12-09 08:57:23 +0100

    Major improvements

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ This is an OBS overlay that show current weather information for Phnom Penh (Cam
 
 ## Attribution
 
-* [Humidity](https://www.flaticon.com/free-icon/humidity_727790) by [Pixel perfect](https://www.flaticon.com/authors/pixel-perfect)
+* [Cloud Day](https://www.flaticon.com/free-icon/cloudy-day_3814929) by [Freepik](https://www.flaticon.com/authors/freepik)
+* [Cloudy Day](https://www.flaticon.com/free-icon/cloudy-day_3814858) by [Freepik](https://www.flaticon.com/authors/freepik)
 * [Compass](https://www.flaticon.com/free-icon/compass_660333) by [Smashicons](https://www.flaticon.com/authors/smashicons)
-* [Sun](https://www.flaticon.com/free-icon/sun_606795) by [Good Ware](https://www.flaticon.com/authors/good-ware)
+* [Haze](https://www.flaticon.com/free-icon/haze_727789) by [Pixel perfect](https://www.flaticon.com/authors/pixel-perfect)
+* [Humidity](https://www.flaticon.com/free-icon/humidity_727790) by [Pixel perfect](https://www.flaticon.com/authors/pixel-perfect)
+* [Rain](https://www.flaticon.com/free-icon/rain_3815058) by [Freepik](https://www.flaticon.com/authors/freepik)
+* [Snowflake](https://www.flaticon.com/free-icon/snowflake_3815117) by [Freepik](https://www.flaticon.com/authors/freepik)
+* [Sunny](https://www.flaticon.com/free-icon/sunny_3815245) by [Freepik](https://www.flaticon.com/authors/freepik)
--- js/script.js
+++ js/script.js
@@ -1,91 +1,117 @@
-// const weatherIcons = {
-//     "Broken clouds": "cloud.gif",
-//     "Clear sky": "cloud.gif",
-//     "Overcast clouds": "cloud.gif",
-//     "Light snow": "cloud.gif",
-//     "Light rain": "cloud.gif",
-//     "Haze": "cloud.gif",
-//     "Scattered clouds": "cloud.gif"
-// }
-
 (function() {
-    const channel = 'dialogikTV';
-
-    // Check if user comes from twitch authentication
-    if(window.location.hash !== '') {
-        // Remove twitch connect link
-        const twitchConnector = document.getElementById('twitch-connector');
-        twitchConnector.parentNode.removeChild(twitchConnector);
-
-        // Extract token
-        var hash = window.location.hash.split('#')[1].split('&')[0].split('=');
-        var token = null;
-        if(hash[0] == 'access_token') {
-            var token = hash[1];
-
-            // Remove token from URL
-            history.pushState("", document.title, window.location.pathname);
-
-            // Connect to chat
-            const { chat } = new window.TwitchJs({ token, channel });
-            chat.connect().then(() => {
-                chat.join(channel);
-            });
-
-            const handleMessage = message => {
-                if(message.event == 'PRIVMSG') {
-                    if(
-                        message.tags.badges.hasOwnProperty('broadcaster') &&
-                        message.tags.badges.broadcaster === '1' &&
-                        message.message === '!wetter'
-                    ) {
-                        const weatherUrl = encodeURI('https://api.scorpstuff.com/weather.php?units=metric&city="Phnom Penh"');
-                        fetch(`https://cors-anywhere.herokuapp.com/${weatherUrl}`)
-                        .then(response => response.text()).then(text => {
-                            // Parse data
-                            const data = parseWeatherData(text);
-                            console.log(data);
-
-                            // Map HTML fields
-                            const temperatureHolder = document.getElementById('temperature-value');
-                            const windHolder = document.getElementById('wind-value');
-                            const humidityHolder = document.getElementById('humidity-value');
-
-                            // Display data
-                            temperatureHolder.innerText = parseInt(data.temperatureC);
-                            windHolder.innerText = parseInt(data.windSpeedKPH);
-                            humidityHolder.innerText = parseInt(data.humidity);
-
-                            const label = document.getElementById('label');
-                            label.classList.add('visible');
-                            setTimeout(function() {
-                                label.classList.remove('visible');
-                            }, 10000);
-                        })
-                        .catch(function (err) {
-                            console.error('Fehler!', err);
-                        });
-                    }
-                }
-            };
-    
-            // Listen for all events.
-            chat.on(TwitchJs.Chat.Events.ALL, handleMessage);
-        }
+    const channel = 'Freakydna';
+    const weatherIcons = {
+        "Few clouds": "broken-clouds.png",
+        "Broken clouds": "broken-clouds.png",
+        "Clear sky": "sun.png",
+        "Overcast clouds": "scattered-clouds.png",
+        "Light snow": "snow.png",
+        "Light rain": "rain.png",
+        "Haze": "haze.png",
+        "Scattered clouds": "scattered-clouds.png"
     }
+
+    // Connect to chat
+    const { chat } = new window.TwitchJs({ channel });
+    chat.connect().then(() => {
+        chat.join(channel);
+    });
+
+    const handleMessage = message => {
+        if(message.event == 'PRIVMSG') {
+            if(
+                message.message.startsWith("!wetter") &&
+                (message.tags.badges.broadcaster === '1' || message.tags.badges.moderator === '1')
+            ) {
+                // Determine requested weather location
+                let location = "Phnom Penh";
+                if(message.message.includes(" ")) {
+                    const parts = message.message.split(" ");
+                    location = parts[1];
+                }
+                const weatherUrl = encodeURI(`https://api.scorpstuff.com/weather.php?units=metric&city=${location}`);
+                fetch(`https://cors-anywhere.herokuapp.com/${weatherUrl}`)
+                .then(response => response.text()).then(text => {
+                    // Parse data
+                    const data = parseWeatherData(text);
+                    console.log(data);
+
+                    // Display city (if not default)
+                    const locationHolder = document.getElementById('location-value');
+                    if(location !== 'Phnom Penh') {
+                        locationHolder.innerText = `${data.location.city} (${data.location.country})`;
+                    } else {
+                        locationHolder.innerText = '';
+                    }
+
+                    // Map HTML fields
+                    const temperatureHolder = document.getElementById('temperature-value');
+                    const windHolder = document.getElementById('wind-value');
+                    const humidityHolder = document.getElementById('humidity-value');
+
+                    // Display data
+                    temperatureHolder.innerText = parseInt(data.temperature.c);
+                    windHolder.innerText = parseInt(data.wind.speed.kph);
+                    humidityHolder.innerText = parseInt(data.humidity);
+
+                    // Justify compass needle
+                    const degrees = {
+                        "North": -45,
+                        "North-East": 0,
+                        "East": 45,
+                        "South-East": 90,
+                        "South": 135,
+                        "South-West": 180,
+                        "West": 225,
+                        "North-West": 270
+                    }
+                    const windIcon = document.getElementById('wind-icon');
+                    windIcon.style.transform = `rotate(${degrees[data.wind.direction]}deg)`;
+
+                    // Set weather icon
+                    let weatherIcon = "broken-clouds.png";
+                    if(weatherIcons.hasOwnProperty(data.weather)) {
+                        weatherIcon = weatherIcons[data.weather];
+                    }
+                    const weatherIconHolder = document.getElementById('weather-icon');
+                    weatherIconHolder.src = `img/${weatherIcon}`;
+
+                    const label = document.getElementById('label');
+                    label.classList.add('visible');
+                    setTimeout(function() {
+                        label.classList.remove('visible');
+                    }, 10000);
+                })
+                .catch(function (err) {
+                    console.error('Fehler!', err);
+                });
+            }
+        }
+    };
+
+    // Listen for all events.
+    chat.on(TwitchJs.Chat.Events.ALL, handleMessage);
 })();
 
 function parseWeatherData(text) {
-    const groups = text.match(/Weather for (.*), (\S+): (.*) with a temperature of (-?[0-9]*\.?[0-9]* C) \((-?[0-9]+.?[0-9]+ F)\)\. Wind is blowing from the (.*) at (-?[0-9]*\.?[0-9]* kph) \((-?[0-9]*\.?[0-9]* mph)\) and the humidity is (-?[0-9]*\.?[0-9]*%)/i);
+    const matches = text.match(/Weather for (.*), (\S+): (.*) with a temperature of (-?[0-9]*\.?[0-9]* C) \((-?[0-9]+.?[0-9]+ F)\)\. Wind is blowing from the (.*) at (-?[0-9]*\.?[0-9]* kph) \((-?[0-9]*\.?[0-9]* mph)\) and the humidity is (-?[0-9]*\.?[0-9]*%)/i);
     return {
-        city: groups[1],
-        country: groups[2],
-        weather: groups[3],
-        temperatureC: groups[4],
-        temperatureF: groups[5],
-        windDirection: groups[6],
-        windSpeedKPH: groups[7],
-        windSpeedMPH: groups[8],
-        humidity: groups[9]
+        location: {
+            city: matches[1],
+            country: matches[2]
+        },
+        weather: matches[3],
+        temperature: {
+            c: matches[4],
+            f: matches[5]
+        },
+        wind: {
+            direction: matches[6],
+            speed: {
+                kph: matches[7],
+                mph: matches[8]
+            }
+        },
+        humidity: matches[9]
     };
 }
