7862d19d94f1d994506f49c3bcc919ef48d96ece
Moritz Hesse <mh@interhesse.com>
2020-12-18 09:09:43 +0100

    Handle errors

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 (function() {
     const channel = 'Freakydna';
     const weatherIcons = {
-        "Few clouds": "broken-clouds.png",
-        "Broken clouds": "broken-clouds.png",
-        "Clear sky": "sun.png",
-        "Overcast clouds": "scattered-clouds.png",
-        "Light snow": "snow.png",
-        "Light rain": "rain.png",
-        "Haze": "haze.png",
-        "Scattered clouds": "scattered-clouds.png"
+        'Few clouds': 'broken-clouds.png',
+        'Broken clouds': 'broken-clouds.png',
+        'Clear sky': 'sun.png',
+        'Overcast clouds': 'scattered-clouds.png',
+        'Light snow': 'snow.png',
+        'Light rain': 'rain.png',
+        'Haze': 'haze.png',
+        'Scattered clouds': 'scattered-clouds.png'
     }
+    
+    const temperatureHolder = document.getElementById('temperature-value');
+    const errorMessage = document.getElementById('error-message');
+    const errorDetails = document.getElementById('error-details');
 
     // Connect to chat
     const { chat } = new window.TwitchJs({ channel });
@@ -20,71 +24,92 @@
     const handleMessage = message => {
         if(message.event == 'PRIVMSG') {
             if(
-                message.message.startsWith("!wetter") &&
-                (message.tags.badges.broadcaster === '1' || message.tags.badges.moderator === '1')
+                message.message.startsWith('!wetter') &&
+                (
+                    message.tags.badges.broadcaster === '1'
+                    || message.tags.badges.moderator === '1'
+                    || ["dialogiktv", "dialogik", "geierfogel"].indexOf(message.username) > -1
+                )
             ) {
                 // Determine requested weather location
-                let location = "Phnom Penh";
-                if(message.message.includes(" ")) {
-                    const parts = message.message.split(" ");
-                    location = parts[1];
+                let location = 'Phnom Penh';
+                if(message.message.includes(' ')) {
+                    location = message.message.split(' ').slice(1).join(' ');
                 }
-                const weatherUrl = encodeURI(`https://api.scorpstuff.com/weather.php?units=metric&city=${location}`);
-                fetch(`https://cors-anywhere.herokuapp.com/${weatherUrl}`)
-                .then(response => response.text()).then(text => {
-                    // Parse data
-                    const data = parseWeatherData(text);
-                    console.log(data);
 
-                    // Display city (if not default)
-                    const locationHolder = document.getElementById('location-value');
-                    if(location !== 'Phnom Penh') {
-                        locationHolder.innerText = `${data.location.city} (${data.location.country})`;
-                    } else {
-                        locationHolder.innerText = '';
-                    }
+                location = replaceUmlaute(location);
 
-                    // Map HTML fields
-                    const temperatureHolder = document.getElementById('temperature-value');
-                    const windHolder = document.getElementById('wind-value');
-                    const humidityHolder = document.getElementById('humidity-value');
+                console.log(`Fetching weather data for location [${location}]`);
+                const weatherUrl = encodeURI(`https://weather-api-cors-proxy.herokuapp.com/weather.php?city=${location}`);
+                console.log({weatherUrl});
+                
+                // Fetch API data
+                fetch(weatherUrl)
+                    .then(response => {
+                        return response.text();
+                    }).then(text => {
+                        if(!text.includes("ERROR")) {
+                            // Parse data
+                            const data = parseWeatherData(text);
 
-                    // Display data
-                    temperatureHolder.innerText = parseInt(data.temperature.c);
-                    windHolder.innerText = parseInt(data.wind.speed.kph);
-                    humidityHolder.innerText = parseInt(data.humidity);
+                            // But only if data is present (else: handle errors)
+                            if(data) {
+                                console.log(data);
 
-                    // Justify compass needle
-                    const degrees = {
-                        "North": -45,
-                        "North-East": 0,
-                        "East": 45,
-                        "South-East": 90,
-                        "South": 135,
-                        "South-West": 180,
-                        "West": 225,
-                        "North-West": 270
-                    }
-                    const windIcon = document.getElementById('wind-icon');
-                    windIcon.style.transform = `rotate(${degrees[data.wind.direction]}deg)`;
+                                // Display city (if not default)
+                                const locationHolder = document.getElementById('location-value');
+                                if(location !== 'Phnom Penh') {
+                                    locationHolder.innerText = `${data.location.city} (${data.location.country})`;
+                                } else {
+                                    locationHolder.innerText = '';
+                                }
 
-                    // Set weather icon
-                    let weatherIcon = "broken-clouds.png";
-                    if(weatherIcons.hasOwnProperty(data.weather)) {
-                        weatherIcon = weatherIcons[data.weather];
-                    }
-                    const weatherIconHolder = document.getElementById('weather-icon');
-                    weatherIconHolder.src = `img/${weatherIcon}`;
+                                // Map HTML fields
+                                const windHolder = document.getElementById('wind-value');
+                                const humidityHolder = document.getElementById('humidity-value');
 
-                    const label = document.getElementById('label');
-                    label.classList.add('visible');
-                    setTimeout(function() {
-                        label.classList.remove('visible');
-                    }, 10000);
-                })
-                .catch(function (err) {
-                    console.error('Fehler!', err);
-                });
+                                // Display data
+                                temperatureHolder.innerText = parseInt(data.temperature.c);
+                                windHolder.innerText = parseInt(data.wind.speed.kph);
+                                humidityHolder.innerText = parseInt(data.humidity);
+
+                                // Justify compass needle
+                                const degrees = {
+                                    'North': -45,
+                                    'North-East': 0,
+                                    'East': 45,
+                                    'South-East': 90,
+                                    'South': 135,
+                                    'South-West': 180,
+                                    'West': 225,
+                                    'North-West': 270
+                                }
+                                const windIcon = document.getElementById('wind-icon');
+                                windIcon.style.transform = `rotate(${degrees[data.wind.direction]}deg)`;
+
+                                // Set weather icon
+                                let weatherIcon = 'broken-clouds.png';
+                                if(weatherIcons.hasOwnProperty(data.weather)) {
+                                    weatherIcon = weatherIcons[data.weather];
+                                }
+                                const weatherIconHolder = document.getElementById('weather-icon');
+                                weatherIconHolder.src = `img/${weatherIcon}`;
+
+                                const label = document.getElementById('label');
+                                label.classList.add('visible');
+                                setTimeout(function() {
+                                    label.classList.remove('visible');
+                                }, 10000);
+                            }
+                        } else {
+                            displayError(label, errorMessage, errorDetails, text);
+                        }
+                    })
+
+                    // Catch errors
+                    .catch(function(e) {
+                        displayError(label, errorMessage, errorDetails, e);
+                    });
             }
         }
     };
@@ -95,6 +120,13 @@
 
 function parseWeatherData(text) {
     const matches = text.match(/Weather for (.*), (\S+): (.*) with a temperature of (-?[0-9]*\.?[0-9]* C) \((-?[0-9]+.?[0-9]+ F)\)\. Wind is blowing from the (.*) at (-?[0-9]*\.?[0-9]* kph) \((-?[0-9]*\.?[0-9]* mph)\) and the humidity is (-?[0-9]*\.?[0-9]*%)/i);
+
+    // Handle errors
+    if(matches === null) {
+        return false;
+    }
+
+    // Or return data as structured object
     return {
         location: {
             city: matches[1],
@@ -114,4 +146,35 @@ function parseWeatherData(text) {
         },
         humidity: matches[9]
     };
+}
+
+function displayError(label, errorMessage, errorDetails, error = null) {
+    if(error !== null) {
+        console.error('An error occured', error);
+        if(typeof error == "string") {
+            errorDetails.innerText = error;
+        } else if(typeof error == "object" && error.hasOwnProperty("message")) {
+            errorDetails.innerText = error.message;
+        }
+    }
+    label.classList.add('error');
+    errorMessage.innerText = 'Fehler... :(';
+    label.classList.add('visible');
+    setTimeout(function() {
+        label.classList.remove('visible');
+        setTimeout(function() {
+            label.classList.remove('error');
+        }, 1000);
+    }, 3000);
+}
+
+function replaceUmlaute(value) {
+    value = value.replace(/Ä/g, 'Ae');
+    value = value.replace(/Ö/g, 'Oe');
+    value = value.replace(/Ü/g, 'Ue');
+    value = value.replace(/ä/g, 'ae');
+    value = value.replace(/ö/g, 'oe');
+    value = value.replace(/ü/g, 'ue');
+    value = value.replace(/ß/g, 'ss');
+    return value;
 }
